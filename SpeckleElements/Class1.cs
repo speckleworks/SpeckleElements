792d49a5b961458c3c6fa5751c3ea455eb25d955
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-03-13 19:27:11 +0000

    🥇 commit

--- SpeckleElements/Class1.cs
+++ SpeckleElements/Class1.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpeckleCore;
+using SpeckleCoreGeometryClasses;
 
 namespace SpeckleElements
 {
-    public class Class1
-    {
-    }
+  public interface ISpeckleElement
+  {
+    Dictionary<string, object> parameters { get; set; }
+  }
+
+  public class GridLine : SpeckleLine, ISpeckleElement
+  {
+    public override string Type { get => "GridLine"; set => base.Type = value; }
+
+    public Dictionary<string, object> parameters { get => Properties; set => Properties = value; }
+  }
 }
---
+++ SpeckleElementsRevit/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpeckleCore;
+using SpeckleElements;
+
+namespace SpeckleElementsRevit
+{
+  public class Initialiser : ISpeckleInitializer
+  {
+    public Initialiser( ) { }
+  }
+
+  public static partial class Conversions
+  {
+    public static GetCurrentDocument( )
+    {
+      return Autodesk.Revit.UI.UIApplication
+    }
+
+    public static Autodesk.Revit.DB.Grid ToNative(this GridLine myGridLine)
+    {
+      return null;
+    }
+  }
+}
