5e9478e6e885ea8159fe335daeaca2135f68c122
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-04-09 08:15:39 +0100

    added beam individual conversion transaction

--- SpeckleElementsRevit/ConversionRoutines/Beam.cs
+++ SpeckleElementsRevit/ConversionRoutines/Beam.cs
@@ -14,47 +14,55 @@ namespace SpeckleElementsRevit
     // TODO
     public static Element ToNative( this Beam myBeam )
     {
-      var (docObj, stateObj) = GetExistingElementByApplicationId( myBeam.ApplicationId, myBeam.Type );
-
-      var baseLine = ( Curve ) SpeckleCore.Converter.Deserialise( myBeam.baseLine );
-      var start = baseLine.GetEndPoint( 0 );
-      var end = baseLine.GetEndPoint( 1 );
-
-      if ( docObj != null )
+      using ( var t = new Transaction( Doc, "Speckle Bake " + myBeam._id ) )
       {
-        var type = Doc.GetElement( docObj.GetTypeId() ) as ElementType;
-        if ( myBeam.beamType != null && ( myBeam.beamType != type.Name || myBeam.beamFamily != type.FamilyName ) )
+        t.Start();
+
+        var (docObj, stateObj) = GetExistingElementByApplicationId( myBeam.ApplicationId, myBeam.Type );
+
+        var baseLine = ( Curve ) SpeckleCore.Converter.Deserialise( myBeam.baseLine );
+        var start = baseLine.GetEndPoint( 0 );
+        var end = baseLine.GetEndPoint( 1 );
+
+        if ( docObj != null )
         {
-          Doc.Delete( docObj.Id );
-          // Will create a new one, exits fully this nested if
+          var type = Doc.GetElement( docObj.GetTypeId() ) as ElementType;
+          if ( myBeam.beamType != null && ( myBeam.beamType != type.Name || myBeam.beamFamily != type.FamilyName ) )
+          {
+            Doc.Delete( docObj.Id );
+            // Will create a new one, exits fully this nested if
+          }
+          else
+          {
+            // Edit location curve
+            var existingFamilyInstance = ( Autodesk.Revit.DB.FamilyInstance ) docObj;
+            var existingLocationCurve = existingFamilyInstance.Location as LocationCurve;
+            existingLocationCurve.Curve = baseLine;
+
+            t.Commit();
+            return existingFamilyInstance;
+          }
         }
-        else
-        {
-          // Edit location curve
-          var existingFamilyInstance = ( Autodesk.Revit.DB.FamilyInstance ) docObj;
-          var existingLocationCurve = existingFamilyInstance.Location as LocationCurve;
-          existingLocationCurve.Curve = baseLine;
-          return existingFamilyInstance;
-        }
+
+        FamilySymbol sym;
+        sym = GetFamilySymbolByFamilyNameAndTypeAndCategory( myBeam.beamFamily, myBeam.beamType, BuiltInCategory.OST_StructuralFraming );
+
+        if ( sym == null )
+          sym = GetFamilySymbolByFamilyNameAndTypeAndCategory( myBeam.beamFamily, myBeam.beamType, BuiltInCategory.OST_BeamAnalytical );
+
+        if ( sym == null )
+          return null;
+
+        if ( myBeam.level == null )
+          myBeam.level = new SpeckleElements.Level() { elevation = 0, levelName = "Speckle Level 0" };
+        var myLevel = myBeam.level.ToNative() as Autodesk.Revit.DB.Level;
+
+        if ( !sym.IsActive ) sym.Activate();
+        var familyInstance = Doc.Create.NewFamilyInstance( baseLine, sym, myLevel, StructuralType.Beam );
+
+        t.Commit();
+        return familyInstance;
       }
-
-      FamilySymbol sym;
-      sym = GetFamilySymbolByFamilyNameAndTypeAndCategory( myBeam.beamFamily, myBeam.beamType, BuiltInCategory.OST_StructuralFraming );
-
-      if ( sym == null )
-        sym = GetFamilySymbolByFamilyNameAndTypeAndCategory( myBeam.beamFamily, myBeam.beamType, BuiltInCategory.OST_BeamAnalytical );
-
-      if ( sym == null )
-        return null;
-
-      if ( myBeam.level == null )
-        myBeam.level = new SpeckleElements.Level() { elevation = 0, levelName = "Speckle Level 0" };
-      var myLevel = myBeam.level.ToNative() as Autodesk.Revit.DB.Level;
-
-      if ( !sym.IsActive ) sym.Activate();
-      var familyInstance = Doc.Create.NewFamilyInstance( baseLine, sym, myLevel, StructuralType.Beam );
-
-      return familyInstance;
     }
   }
 }
