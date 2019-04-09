f35ca995ecda31694801e1af67296809992ff195
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-04-09 12:40:15 +0100

    moved transaction up in bake for loop

--- SpeckleElementsRevit/ConversionRoutines/Beam.cs
+++ SpeckleElementsRevit/ConversionRoutines/Beam.cs
@@ -14,55 +14,47 @@ namespace SpeckleElementsRevit
     // TODO
     public static Element ToNative( this Beam myBeam )
     {
-      using ( var t = new Transaction( Doc, "Speckle Bake " + myBeam._id ) )
+      var (docObj, stateObj) = GetExistingElementByApplicationId( myBeam.ApplicationId, myBeam.Type );
+
+      var baseLine = ( Curve ) SpeckleCore.Converter.Deserialise( myBeam.baseLine );
+      var start = baseLine.GetEndPoint( 0 );
+      var end = baseLine.GetEndPoint( 1 );
+
+      if ( docObj != null )
       {
-        t.Start();
-
-        var (docObj, stateObj) = GetExistingElementByApplicationId( myBeam.ApplicationId, myBeam.Type );
-
-        var baseLine = ( Curve ) SpeckleCore.Converter.Deserialise( myBeam.baseLine );
-        var start = baseLine.GetEndPoint( 0 );
-        var end = baseLine.GetEndPoint( 1 );
-
-        if ( docObj != null )
+        var type = Doc.GetElement( docObj.GetTypeId() ) as ElementType;
+        if ( myBeam.beamType != null && ( myBeam.beamType != type.Name || myBeam.beamFamily != type.FamilyName ) )
         {
-          var type = Doc.GetElement( docObj.GetTypeId() ) as ElementType;
-          if ( myBeam.beamType != null && ( myBeam.beamType != type.Name || myBeam.beamFamily != type.FamilyName ) )
-          {
-            Doc.Delete( docObj.Id );
-            // Will create a new one, exits fully this nested if
-          }
-          else
-          {
-            // Edit location curve
-            var existingFamilyInstance = ( Autodesk.Revit.DB.FamilyInstance ) docObj;
-            var existingLocationCurve = existingFamilyInstance.Location as LocationCurve;
-            existingLocationCurve.Curve = baseLine;
-
-            t.Commit();
-            return existingFamilyInstance;
-          }
+          Doc.Delete( docObj.Id );
+          // Will create a new one, exits fully this nested if
         }
-
-        FamilySymbol sym;
-        sym = GetFamilySymbolByFamilyNameAndTypeAndCategory( myBeam.beamFamily, myBeam.beamType, BuiltInCategory.OST_StructuralFraming );
-
-        if ( sym == null )
-          sym = GetFamilySymbolByFamilyNameAndTypeAndCategory( myBeam.beamFamily, myBeam.beamType, BuiltInCategory.OST_BeamAnalytical );
-
-        if ( sym == null )
-          return null;
-
-        if ( myBeam.level == null )
-          myBeam.level = new SpeckleElements.Level() { elevation = 0, levelName = "Speckle Level 0" };
-        var myLevel = myBeam.level.ToNative() as Autodesk.Revit.DB.Level;
-
-        if ( !sym.IsActive ) sym.Activate();
-        var familyInstance = Doc.Create.NewFamilyInstance( baseLine, sym, myLevel, StructuralType.Beam );
-
-        t.Commit();
-        return familyInstance;
+        else
+        {
+          // Edit location curve
+          var existingFamilyInstance = ( Autodesk.Revit.DB.FamilyInstance ) docObj;
+          var existingLocationCurve = existingFamilyInstance.Location as LocationCurve;
+          existingLocationCurve.Curve = baseLine;
+          return existingFamilyInstance;
+        }
       }
+
+      FamilySymbol sym;
+      sym = GetFamilySymbolByFamilyNameAndTypeAndCategory( myBeam.beamFamily, myBeam.beamType, BuiltInCategory.OST_StructuralFraming );
+
+      if ( sym == null )
+        sym = GetFamilySymbolByFamilyNameAndTypeAndCategory( myBeam.beamFamily, myBeam.beamType, BuiltInCategory.OST_BeamAnalytical );
+
+      if ( sym == null )
+        return null;
+
+      if ( myBeam.level == null )
+        myBeam.level = new SpeckleElements.Level() { elevation = 0, levelName = "Speckle Level 0" };
+      var myLevel = myBeam.level.ToNative() as Autodesk.Revit.DB.Level;
+
+      if ( !sym.IsActive ) sym.Activate();
+      var familyInstance = Doc.Create.NewFamilyInstance( baseLine, sym, myLevel, StructuralType.Beam );
+
+      return familyInstance;
     }
   }
 }
