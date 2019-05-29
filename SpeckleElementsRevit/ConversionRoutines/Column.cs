a7d9ac326eb2c2880a1093a43b420924474ec5b7
Dimitrie Stefanescu <didimitrie@gmail.com>
2019-05-29 17:48:03 +0100

    matching flipping on columns. next step: rotation

--- SpeckleElementsRevit/ConversionRoutines/Column.cs
+++ SpeckleElementsRevit/ConversionRoutines/Column.cs
@@ -21,13 +21,20 @@ namespace SpeckleElementsRevit
 
       // get family symbol; it's used throughout
       FamilySymbol familySymbol = TryGetColumnFamilySymbol( myCol.columnFamily, myCol.columnType );
-      
+
+      // Freak out if we don't have a symbol.
+      if( familySymbol == null )
+        return null;
+
+      // Activate the symbol yo! 
+      if( !familySymbol.IsActive ) familySymbol.Activate();
+
       if( docObj != null )
       {
         var type = Doc.GetElement( docObj.GetTypeId() ) as ElementType;
 
         // if family changed, tough luck - delete and rewind
-        if( myCol.columnFamily != type.FamilyName) 
+        if( myCol.columnFamily != type.FamilyName )
         {
           Doc.Delete( docObj.Id );
         }
@@ -40,52 +47,52 @@ namespace SpeckleElementsRevit
           existingLocationCurve.Curve = baseLine;
 
           // check if type changed, and try and change it
-          if( myCol.columnType != null && (myCol.columnType != type.Name))
+          if( myCol.columnType != null && (myCol.columnType != type.Name) )
           {
             existingFamilyInstance.ChangeTypeId( familySymbol.Id );
           }
-          
+
+          // Final preparations for good measure
+          MatchFlipping( existingFamilyInstance, myCol );
           SetElementParams( existingFamilyInstance, myCol.parameters );
           return existingFamilyInstance;
         }
       }
 
-      // below, new creation of a column.
-      // fake out if we don't have a symbol.
-      if( familySymbol == null )
-        return null;
-
-
-      if( !familySymbol.IsActive ) familySymbol.Activate();
-
-      // Set base level
+      // Create base level
       if( myCol.baseLevel == null )
         myCol.baseLevel = new SpeckleElements.Level() { elevation = baseLine.GetEndPoint( 0 ).Z / Scale, levelName = "Speckle Level " + baseLine.GetEndPoint( 0 ).Z / Scale };
       var baseLevel = myCol.baseLevel.ToNative() as Autodesk.Revit.DB.Level;
 
-     
+
+      // Create the family instance
       var familyInstance = Doc.Create.NewFamilyInstance( start, familySymbol, baseLevel, Autodesk.Revit.DB.Structure.StructuralType.Column );
 
+      // Make it slanted to avoid potential errors when not straight
       familyInstance.get_Parameter( BuiltInParameter.SLANTED_COLUMN_TYPE_PARAM ).Set( (double) SlantedOrVerticalColumnType.CT_EndPoint );
 
+      // Set the top level
       if( myCol.topLevel != null )
       {
         var myTopLevel = myCol.topLevel.ToNative();
         familyInstance.get_Parameter( BuiltInParameter.FAMILY_TOP_LEVEL_PARAM ).Set( myTopLevel.Id );
       }
 
+      // Set the location curve
       var locationCurve = familyInstance.Location as LocationCurve;
       locationCurve.Curve = baseLine;
 
+      // Final preparations
+      MatchFlipping( familyInstance, myCol );
       SetElementParams( familyInstance, myCol.parameters );
 
       return familyInstance;
     }
 
-    public static FamilySymbol TryGetColumnFamilySymbol(string columnFamily, string columnType )
+    public static FamilySymbol TryGetColumnFamilySymbol( string columnFamily, string columnType )
     {
       FamilySymbol sym;
-      sym = GetFamilySymbolByFamilyNameAndTypeAndCategory(columnFamily, columnType, BuiltInCategory.OST_StructuralColumns );
+      sym = GetFamilySymbolByFamilyNameAndTypeAndCategory( columnFamily, columnType, BuiltInCategory.OST_StructuralColumns );
 
       if( sym == null )
       {
@@ -98,6 +105,25 @@ namespace SpeckleElementsRevit
       }
 
       return sym;
+    }
+
+    public static void MatchFlipping( Autodesk.Revit.DB.FamilyInstance myInstance, Column myColumn )
+    {
+      try
+      {
+        var handFlip = Convert.ToBoolean( myColumn.Properties[ "__handFlipped" ] );
+        if( handFlip != myInstance.HandFlipped )
+          myInstance.flipHand();
+      }
+      catch { }
+
+      try
+      {
+        var faceFlip = Convert.ToBoolean( myColumn.Properties[ "__facingFlipped" ] );
+        if( faceFlip != myInstance.FacingFlipped )
+          myInstance.flipFacing();
+      }
+      catch { }
     }
 
     public static Column ColumnToSpeckle( Autodesk.Revit.DB.FamilyInstance myFamily )
@@ -117,8 +143,8 @@ namespace SpeckleElementsRevit
       myColumn.baseLevel = baseLevel?.ToSpeckle();
       myColumn.topLevel = topLevel?.ToSpeckle();
 
-      myColumn.Properties[ "facingFlipped" ] = myFamily.FacingFlipped;
-      myColumn.Properties[ "handFlipped" ] = myFamily.HandFlipped;
+      myColumn.Properties[ "__facingFlipped" ] = myFamily.FacingFlipped;
+      myColumn.Properties[ "__handFlipped" ] = myFamily.HandFlipped;
 
       myColumn.GenerateHash();
       myColumn.ApplicationId = myFamily.UniqueId;
